e1a994e16e6f72c7049cf236aa824fd7172d0612
figgy891 <figgy891@gmail.com>
2021-03-30 22:46:10 -0500

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import { Button, List, Collapse, Breadcrumb } from 'antd';
 const { Panel } = Collapse;
@@ -9,44 +9,52 @@ const { Panel } = Collapse;
 
 function App() {
 
-  const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState();
-  const [questions, setQuestions] = useState();
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState();
+    const [selectedQuestion, setSelectedQuestion] = useState();
+    const [questions, setQuestions] = useState();
 
-  const [questionTxt, setQuestionTxt] = useState('');
+    const [questionTxt, setQuestionTxt] = useState('');
+    const [answerTxt, setAnswerTxt] = useState('');
+    const [answers, setAnswers] = useState();
 
-  let apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+    let apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
 
-  const fetchCategories = async () => {
-    console.log('this will fetch the categories');
+    const fetchCategories = async () => {
+        console.log('this will fetch the categories');
 
-    // let res = await fetch('http://localhost:3000/api/v1/categories');
-    //   https://cohort11a-capstone-api.herokuapp.com
-    //   console.log(process.env.API_URL)
-    //   console.log(process.env.REACT_APP_API_URL)
-      console.log(`${apiUrl}/api/v1/categories`)
-    let res = await fetch(`${apiUrl}/api/v1/categories`);
-    let data = await res.json();
-    console.log(data);
-    setCategories(data);
-  };
+        // let res = await fetch('http://localhost:3000/api/v1/categories');
+        //   https://cohort11a-capstone-api.herokuapp.com
+        //   console.log(process.env.API_URL)
+        //   console.log(process.env.REACT_APP_API_URL)
+        console.log(`${apiUrl}/api/v1/categories`)
+        let res = await fetch(`${apiUrl}/api/v1/categories`);
+        let data = await res.json();
+        console.log(data);
+        setCategories(data);
+    };
 
-  useEffect(() => {
-    // this code will run only once on component mount
-    fetchCategories()
-  }, [])
+    useEffect(() => {
+        // this code will run only once on component mount
+        fetchCategories()
+    }, [])
 
     useEffect(() => {
         // this code is going to run whenever the selectedCategory changes
         // fetchQuestions() TheTodo: fetch and show the questions
     }, [selectedCategory])
 
-  // useEffect(() => {
-  //   // this code will run every time the someStateVariable changes
-  //   // this code will run every time var2 OR someStateVariable changes
-  //   // write code here that reloads the page as a side effect of var2 OR someStateVariable changing
-  // }, [someStateVariable, var2])
+    useEffect(() => {
+        // this code is going to run whenever the selectedCategory changes
+        // fetchQuestions() TheTodo: fetch and show the questions
+    }, [selectedQuestion])
+
+    // useEffect(() => {
+    //   // this code will run every time the someStateVariable changes
+    //   // this code will run every time var2 OR someStateVariable changes
+    //   // write code here that reloads the page as a side effect of var2 OR someStateVariable changing
+    // }, [someStateVariable, var2])
 
 
     const fetchQuestionsForCategory = async (id) => {
@@ -59,159 +67,198 @@ function App() {
 
     };
 
-  const createNewQuestion = async () => {
-      console.log('create a question for the category id', selectedCategory)
-      let res = await fetch(`${apiUrl}/api/v1/categories/${selectedCategory}/questions`, {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-              // 'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          body: JSON.stringify({questionTxt: questionTxt})
-      });
-      fetchQuestionsForCategory(selectedCategory);
-      setQuestionTxt('')
+    const fetchAnswersForQuestion = async (id) => {
+        console.log('fetch answers for this question id', id);
+        let res = await fetch(`http://localhost:3000/api/v1/categories/:categoryId/questions/${id}/answers`);
+        let data = await res.json();
+        console.log(data);
+        setAnswers(data);
+        // setCategories(data);
 
-      // the usual fetch request / HINT : look up the stock API request
-      // 1. Make a POST request to create a question
-      // 2. Once the call is successful
-      // 3. Fetch the questions for a category again (reload the questions)
-      // 4. done!
-  };
+    };
 
+    const createNewQuestion = async () => {
+        console.log('create a question for the category id', selectedCategory)
+        let res = await fetch(`${apiUrl}/api/v1/categories/${selectedCategory}/questions`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({ questionTxt: questionTxt })
+        });
+        fetchQuestionsForCategory(selectedCategory);
+        setQuestionTxt('')
 
-  const createANewAnswer = async () => {
-
-      // you will need something called selectedQuestion to keep a track of the question that has been selected
-      // a state variable to store the answer text that the user types in
-
-      // the usual fetch request / HINT : look up the stock API request
-      // 1. Make a POST request to create an answer
-      // 2. Once the call is successful
-      // 3. Fetch the questions for a category again (reload the questions)
-      // 4. done!
-
-  };
+        // the usual fetch request / HINT : look up the stock API request
+        // 1. Make a POST request to create a question
+        // 2. Once the call is successful
+        // 3. Fetch the questions for a category again (reload the questions)
+        // 4. done!
+    };
 
 
-  return (
-    <>
-      <div className="grid grid-cols-12">
-        <div className={'col-span-full border p-5'}>
-          <h1 className={'text-center text-3xl'}>Questions App</h1>
-        </div>
+    const createANewAnswer = async () => {
+        console.log('create an answer for the question id', selectedQuestion)
+        let res = await fetch(`${apiUrl}/api/v1/categories/:categoryId/questions/${selectedQuestion}/answers`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({ answerTxt: answerTxt })
+        });
+        fetchAnswersForQuestion(selectedQuestion);
+        setAnswerTxt('')
+        // you will need something called selectedQuestion to keep a track of the question that has been selected
+        // a state variable to store the answer text that the user types in
 
-      </div>
+        // the usual fetch request / HINT : look up the stock API request
+        // 1. Make a POST request to create an answer
+        // 2. Once the call is successful
+        // 3. Fetch the questions for a category again (reload the questions)
+        // 4. done!
 
-      <div className="grid grid-cols-12">
-        <div className={'col-span-full md:col-span-3 lg:col-span-2 border p-5'}>
+    };
+
+    const handleKeypress = (e) => {
+        //it triggers by pressing the enter key
+      if (e.key === 'Enter' && questionTxt) {
+        createNewQuestion();
+      }
+      if (e.key === 'Enter' && answerTxt) {
+        createANewAnswer();
+      }
+    };
 
 
-            {/*<Button type={'primary'}>Submit</Button>*/}
-          {/*<h1 className={'text-center text-3xl'}>Currently selected category is: {selectedCategory}</h1>*/}
-          {/*  <ul>*/}
-          {/*      {categories.map((category, index) => {*/}
-          {/*          return <li key={index} className={category.id == selectedCategory ? 'border p-5 cursor-pointer bg-gray-200' : 'border p-5 cursor-pointer'} onClick={() => {*/}
-          {/*              setSelectedCategory(category.id);*/}
-          {/*              fetchQuestionsForCategory(category.id)*/}
-          {/*          }}>*/}
-          {/*              {category.name}*/}
-          {/*          </li>*/}
-          {/*      })}*/}
-          {/*  </ul>*/}
+    return (
+        <>
+            <div className="grid grid-cols-12">
+                <div className={'col-span-full border p-5'}>
+                    <h1 className={'text-center text-3xl'}>Questions App</h1>
+                </div>
 
-            <List
-                size="large"
-                header={<div className={'font-bold'}>Categories List</div>}
-                // footer={<div>Footer</div>}
-                bordered
-                dataSource={categories}
-                renderItem={category => <List.Item>
-                    <div className={category.id == selectedCategory ? 'cursor-pointer text-blue-500 font-bold' : 'cursor-pointer'} onClick={() => {
-                        setSelectedCategory(category.id);
-                        fetchQuestionsForCategory(category.id)
+            </div>
+
+            <div className="grid grid-cols-12">
+                <div className={'col-span-full md:col-span-3 lg:col-span-2 border p-5'}>
+
+
+                    {/*<Button type={'primary'}>Submit</Button>*/}
+                    {/*<h1 className={'text-center text-3xl'}>Currently selected category is: {selectedCategory}</h1>*/}
+                    {/*  <ul>*/}
+                    {/*      {categories.map((category, index) => {*/}
+                    {/*          return <li key={index} className={category.id == selectedCategory ? 'border p-5 cursor-pointer bg-gray-200' : 'border p-5 cursor-pointer'} onClick={() => {*/}
+                    {/*              setSelectedCategory(category.id);*/}
+                    {/*              fetchQuestionsForCategory(category.id)*/}
+                    {/*          }}>*/}
+                    {/*              {category.name}*/}
+                    {/*          </li>*/}
+                    {/*      })}*/}
+                    {/*  </ul>*/}
+
+                    <List
+                        size="large"
+                        header={<div className={'font-bold'}>Categories List</div>}
+                        // footer={<div>Footer</div>}
+                        bordered
+                        dataSource={categories}
+                        renderItem={category => <List.Item>
+                            <div className={category.id == selectedCategory ? 'cursor-pointer text-blue-500 font-bold' : 'cursor-pointer'} onClick={() => {
+                                setSelectedCategory(category.id);
+                                fetchQuestionsForCategory(category.id)
+                            }}>
+                                {category.name}
+                            </div>
+
+                        </List.Item>}
+                    />
+
+
+                </div>
+
+                <div className={'col-span-full md:col-span-9 lg:col-span-10 border p-5'}>
+
+                    {/*<button className={'border p-2 pl-4 pr-4 bg-gray-200'} onClick={createNewQuestion}>New Question</button>*/}
+
+                    {/*<Breadcrumb>*/}
+                    {/*    <Breadcrumb.Item>Home</Breadcrumb.Item>*/}
+                    {/*    <Breadcrumb.Item>*/}
+                    {/*        <a href="">Application Center</a>*/}
+                    {/*    </Breadcrumb.Item>*/}
+                    {/*    <Breadcrumb.Item>*/}
+                    {/*        <a href="">Application List</a>*/}
+                    {/*    </Breadcrumb.Item>*/}
+                    {/*    <Breadcrumb.Item>An Application</Breadcrumb.Item>*/}
+                    {/*</Breadcrumb>*/}
+
+                    {selectedCategory && <div>
+                        <input value={questionTxt} onKeyPress={handleKeypress} onChange={(ev) => {
+                            setQuestionTxt(ev.currentTarget.value);
+                        }}  type="text" className={'border p-1 mr-5 w-2/3'} />
+                        <Button type={'primary'} onClick={createNewQuestion}>Create new question</Button>
+                        <br />
+                        <br />
+                    </div>}
+
+
+                    {/*<ul>*/}
+                    {/*    {questions && questions.map((question) => {*/}
+                    {/*        return <li key={question.id}>*/}
+                    {/*            /!*{question.questionTxt} - {question.Answers.length}*!/*/}
+                    {/*            {question.questionTxt} {question.Answers.length >0 && <span>- <span>{question.Answers.length}</span></span>}*/}
+                    {/*        </li>*/}
+                    {/*    })}*/}
+                    {/*</ul>*/}
+
+                    {selectedCategory && <Collapse accordion onChange={(activeKey) => {
+                        setSelectedQuestion(activeKey); fetchAnswersForQuestion(activeKey)
+
                     }}>
-                        {category.name}
-                    </div>
 
-                </List.Item>}
-            />
+                        {questions && questions.map((question, index) => {
+                            return <Panel header={question.questionTxt} key={question.id} >
 
 
-        </div>
+                                <List
+                                    size="small"
+                                    // header={<div className={'font-bold'}>Answers List</div>}
 
-        <div className={'col-span-full md:col-span-9 lg:col-span-10 border p-5'}>
+                                    footer={<div>
+                                        <input value={answerTxt} onKeyPress={handleKeypress} onChange={(ev) => {
+                                            setAnswerTxt(ev.currentTarget.value);
+                                        }} type="text" className={'border p-1 mr-5 w-2/3'} />
+                                        <Button type={'primary'} onClick={createANewAnswer}>Add Answer</Button>
+                                    </div>}
+                                    bordered
+                                    dataSource={answers}
+                                    //dataSource={question.Answers}
+                                    renderItem={answer => <List.Item>
+                                        <div>
+                                            {answer.answerTxt}
+                                        </div>
 
-            {/*<button className={'border p-2 pl-4 pr-4 bg-gray-200'} onClick={createNewQuestion}>New Question</button>*/}
-
-            {/*<Breadcrumb>*/}
-            {/*    <Breadcrumb.Item>Home</Breadcrumb.Item>*/}
-            {/*    <Breadcrumb.Item>*/}
-            {/*        <a href="">Application Center</a>*/}
-            {/*    </Breadcrumb.Item>*/}
-            {/*    <Breadcrumb.Item>*/}
-            {/*        <a href="">Application List</a>*/}
-            {/*    </Breadcrumb.Item>*/}
-            {/*    <Breadcrumb.Item>An Application</Breadcrumb.Item>*/}
-            {/*</Breadcrumb>*/}
-
-            {selectedCategory && <div>
-                <input value={questionTxt} onChange={(ev) => {
-                    setQuestionTxt(ev.currentTarget.value);
-                }} type="text" className={'border p-1 mr-5 w-2/3'}/>
-                <Button type={'primary'} onClick={createNewQuestion}>Create new question</Button>
-                <br/>
-                <br/>
-            </div>}
+                                    </List.Item>}
+                                />
 
 
-            {/*<ul>*/}
-            {/*    {questions && questions.map((question) => {*/}
-            {/*        return <li key={question.id}>*/}
-            {/*            /!*{question.questionTxt} - {question.Answers.length}*!/*/}
-            {/*            {question.questionTxt} {question.Answers.length >0 && <span>- <span>{question.Answers.length}</span></span>}*/}
-            {/*        </li>*/}
-            {/*    })}*/}
-            {/*</ul>*/}
 
-            {selectedCategory && <Collapse accordion>
-                {questions && questions.map((question, index) => {
-                    return <Panel header={question.questionTxt} key={index}>
+                            </Panel>
+                        })}
 
 
-                        <List
-                            size="small"
-                            // header={<div className={'font-bold'}>Answers List</div>}
-                            footer={<div>
-                                <input value={questionTxt} onChange={(ev) => {
-                                    setQuestionTxt(ev.currentTarget.value);
-                                }} type="text" className={'border p-1 mr-5 w-2/3'}/>
-                                <Button type={'primary'} onClick={createANewAnswer}>Add Answer</Button>
-                            </div>}
-                            bordered
-                            dataSource={question.Answers}
-                            renderItem={answer => <List.Item>
-                                <div>
-                                    {answer.answerTxt}
-                                </div>
+                    </Collapse>}
 
-                            </List.Item>}
-                        />
+                    {!selectedCategory && <h1 className={'text-center text-xl uppercase tracking-wider text-blue-500'}>Select a category to get started</h1>}
 
+                    {/*{questions && <p>{JSON.stringify(questions)}</p>}*/}
+                </div>
 
-                    </Panel>
-                })}
-            </Collapse>}
+            </div>
 
-            {!selectedCategory && <h1 className={'text-center text-xl uppercase tracking-wider text-blue-500'}>Select a category to get started</h1>}
-
-            {/*{questions && <p>{JSON.stringify(questions)}</p>}*/}
-        </div>
-
-      </div>
-
-      </>
-  );
+        </>
+    );
 }
 
 export default App;
